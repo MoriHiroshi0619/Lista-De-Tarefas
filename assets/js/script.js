09b863f4f2d2407805b32f8fe54b47c3f678edf7
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-02-14 01:18:32 -0400

    updated
    projeto quase pronto.
    falta implementar a versão mobile e adicionar o tal temido localStorage.
    além é claro de colocar um header pra ficar bonitinho

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
 let btnAddTarefa = document.querySelector('.btn-add-tarefa');
 let listaTarefas = document.querySelector('.listaTarefas');
+let janenaEditFundo = document.querySelector('#janelaEditFundo');
+let jenalaEdit = document.querySelector('#janelaEdit');
+let idTarefaEdicao = document.querySelector('#idTarefaEdicao');
 
 inputNovaTarefa.addEventListener('keypress', (e) => {
     if(inputNovaTarefa.value.length == 0){
@@ -70,9 +73,46 @@ function criarTagLi(tarefa){
 function editar(idTarefa){
     let li = document.getElementById(idTarefa);
     if(li){
-        
+        janenaEditFundo.classList.add('abrir');
+        jenalaEdit.classList.add('abrir');
+
+        let inputEdit = document.querySelector('#inputEdit');
+        inputEdit.value = li.innerText;
+
+        idTarefaEdicao.innerHTML = '#' + idTarefa;
+
+        let fechar = document.querySelector('.btn-fechar');
+        fechar.setAttribute('onclick', 'fecharJanelaEdit()');
+
+        let salvar = document.querySelector('#btn-salvar');
+
+        salvar.addEventListener('click', (e) => {
+            e.preventDefault();
+            let idTarefa = idTarefaEdicao.innerHTML.replace('#', '');
+            let tarefa = {
+                nome: inputEdit.value,
+                id: idTarefa,
+            }
+            let tarefaAtual = document.getElementById(idTarefa);
+
+            if(tarefaAtual){
+                let li = criarTagLi(tarefa);
+                listaTarefas.replaceChild(li, tarefaAtual);
+                fecharJanelaEdit();
+            }
+        })
+
+    }else{
+        alert('elemento HTMl não encontrado')
     }
 }
+
+
+function fecharJanelaEdit(){
+    janenaEditFundo.classList.remove('abrir');
+    jenalaEdit.classList.remove('abrir');
+}
+
 
 function deletar(idTarefa){
     let confirma = window.confirm('Tem certeza que deseja excluir?')
@@ -80,6 +120,9 @@ function deletar(idTarefa){
         let li = document.getElementById(idTarefa);
         if(li){
             listaTarefas.removeChild(li);
+        }
+        else{
+            alert('elemento HTMl não encontrado')
         }
     }
 }
